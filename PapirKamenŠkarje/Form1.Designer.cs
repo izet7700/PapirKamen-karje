643088ca9d26422a6b7c15b89090caf8fe9118cf
izet7700 <43537592+izet7700@users.noreply.github.com>
2019-02-26 11:08:00 +0100

    Initial commit

---
+++ PapirKamenŠkarje/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace PapirKamenŠkarje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.igralec1PictureBox = new System.Windows.Forms.PictureBox();
+            this.igralec2PictureBox = new System.Windows.Forms.PictureBox();
+            this.igralec1Label = new System.Windows.Forms.Label();
+            this.igralec2Label = new System.Windows.Forms.Label();
+            this.igralec1highLabel = new System.Windows.Forms.Label();
+            this.igralec2highLabel = new System.Windows.Forms.Label();
+            this.igrajButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.igralec1PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.igralec2PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // igralec1PictureBox
+            // 
+            this.igralec1PictureBox.Location = new System.Drawing.Point(27, 60);
+            this.igralec1PictureBox.Name = "igralec1PictureBox";
+            this.igralec1PictureBox.Size = new System.Drawing.Size(249, 305);
+            this.igralec1PictureBox.TabIndex = 0;
+            this.igralec1PictureBox.TabStop = false;
+            // 
+            // igralec2PictureBox
+            // 
+            this.igralec2PictureBox.Location = new System.Drawing.Point(283, 60);
+            this.igralec2PictureBox.Name = "igralec2PictureBox";
+            this.igralec2PictureBox.Size = new System.Drawing.Size(247, 305);
+            this.igralec2PictureBox.TabIndex = 1;
+            this.igralec2PictureBox.TabStop = false;
+            // 
+            // igralec1Label
+            // 
+            this.igralec1Label.AutoSize = true;
+            this.igralec1Label.Location = new System.Drawing.Point(95, 24);
+            this.igralec1Label.Name = "igralec1Label";
+            this.igralec1Label.Size = new System.Drawing.Size(54, 13);
+            this.igralec1Label.TabIndex = 2;
+            this.igralec1Label.Text = "Igralec 1: ";
+            // 
+            // igralec2Label
+            // 
+            this.igralec2Label.AutoSize = true;
+            this.igralec2Label.Location = new System.Drawing.Point(378, 24);
+            this.igralec2Label.Name = "igralec2Label";
+            this.igralec2Label.Size = new System.Drawing.Size(54, 13);
+            this.igralec2Label.TabIndex = 3;
+            this.igralec2Label.Text = "Igralec 2: ";
+            // 
+            // igralec1highLabel
+            // 
+            this.igralec1highLabel.AutoSize = true;
+            this.igralec1highLabel.Location = new System.Drawing.Point(44, 404);
+            this.igralec1highLabel.Name = "igralec1highLabel";
+            this.igralec1highLabel.Size = new System.Drawing.Size(105, 13);
+            this.igralec1highLabel.TabIndex = 4;
+            this.igralec1highLabel.Text = "Igralec 1 Highscore: ";
+            // 
+            // igralec2highLabel
+            // 
+            this.igralec2highLabel.AutoSize = true;
+            this.igralec2highLabel.Location = new System.Drawing.Point(378, 404);
+            this.igralec2highLabel.Name = "igralec2highLabel";
+            this.igralec2highLabel.Size = new System.Drawing.Size(108, 13);
+            this.igralec2highLabel.TabIndex = 5;
+            this.igralec2highLabel.Text = "Igralec 2 Highscore:  ";
+            // 
+            // igrajButton
+            // 
+            this.igrajButton.Location = new System.Drawing.Point(201, 399);
+            this.igrajButton.Name = "igrajButton";
+            this.igrajButton.Size = new System.Drawing.Size(75, 23);
+            this.igrajButton.TabIndex = 6;
+            this.igrajButton.Text = "Igraj";
+            this.igrajButton.UseVisualStyleBackColor = true;
+            this.igrajButton.Click += new System.EventHandler(this.igrajButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(282, 399);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 450);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.igrajButton);
+            this.Controls.Add(this.igralec2highLabel);
+            this.Controls.Add(this.igralec1highLabel);
+            this.Controls.Add(this.igralec2Label);
+            this.Controls.Add(this.igralec1Label);
+            this.Controls.Add(this.igralec2PictureBox);
+            this.Controls.Add(this.igralec1PictureBox);
+            this.Name = "Form1";
+            this.Text = "Papir Kamen Škarje";
+            ((System.ComponentModel.ISupportInitialize)(this.igralec1PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.igralec2PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox igralec1PictureBox;
+        private System.Windows.Forms.PictureBox igralec2PictureBox;
+        private System.Windows.Forms.Label igralec1Label;
+        private System.Windows.Forms.Label igralec2Label;
+        private System.Windows.Forms.Label igralec1highLabel;
+        private System.Windows.Forms.Label igralec2highLabel;
+        private System.Windows.Forms.Button igrajButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ PapirKamenŠkarje/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PapirKamenŠkarje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Game igra = new Game();
+        private void igrajButton_Click(object sender, EventArgs e)
+        {
+            int player = igra.Play();
+            if (igra.Player1State == 0)
+            {
+                igralec1PictureBox.Image=Properties.Resources.paper; 
+            }
+            else if (igra.Player1State == 1)
+            {
+                igralec1PictureBox.Image = Properties.Resources.rock;
+            }
+            else
+            {
+                igralec1PictureBox.Image = Properties.Resources.scissors;
+            }
+            if (igra.Player2State == 0)
+            {
+                igralec2PictureBox.Image = Properties.Resources.paper;
+            }
+            else if (igra.Player2State == 1)
+            {
+                igralec2PictureBox.Image = Properties.Resources.rock;
+            }
+            else { igralec2PictureBox.Image = Properties.Resources.scissors; }
+
+            if (player == 1) MessageBox.Show("Igralec 1 je zmagal!");
+            else if (player == 2) MessageBox.Show("Igralec 2 je zmagal!");
+            igralec1Label.Text = "Igralec 1: " + igra.Player1Score;
+            igralec1highLabel.Text = "Igralec 1 Highscore: " + Game.Player1Highscore;
+            igralec2Label.Text = "Igralec 2: " + igra.Player2Score;
+            igralec2highLabel.Text = "Igralec 2: " + Game.Player2Highscore;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            igra.Player1Score = 0;
+            igra.Player2Score = 0;
+            igralec1Label.Text = "Igralec 1: " + igra.Player1Score;
+            igralec2Label.Text = "Igralec 2: " + igra.Player2Score;
+        }
+    }
+    public class HelperClass
+    {
+        public static Random rand = new Random();
+        public HelperClass() { }
+        public static int GetState()
+        {
+            return rand.Next(0, 3);
+        }
+    }
+    public class Game
+    {
+        private int p1st, p2st;
+        public int Player1Score { get; set; }
+        public int Player2Score { get; set; }
+        public int Player1State { get { return p1st; } set { if (value >= 0 && value <= 2) p1st = value; } }
+        public int Player2State { get { return p2st; } set { if (value >= 0 && value <= 2) p2st = value; } }
+        public static int Player1Highscore { get; set; }
+        public static int Player2Highscore { get; set; }
+        public Game() { }
+        public int Play()
+        {
+            int zmag;
+            Player1State = HelperClass.GetState();
+            Player2State = HelperClass.GetState();
+            #region randState
+            if (Player1State == Player2State)
+            {
+                zmag = 0;
+            }
+            else if(Player1State==0 && Player2State == 1)
+            {
+                zmag = 1;
+                Player1Score++;
+                if (Player1Score > Player1Highscore) Player1Highscore = Player1Score;
+            }
+            else if(Player2State == 0 && Player1State == 1)
+            {
+                zmag = 2;
+                Player2Score++;
+                if (Player2Score > Player2Highscore) Player2Highscore = Player2Score;
+            }
+            else if(Player1State==0 && Player2State == 2)
+            {
+                zmag = 2;
+                Player2Score++;
+                if (Player2Score > Player2Highscore) Player2Highscore = Player2Score;
+            }
+            else if(Player2State==0 && Player1State == 2)
+            {
+                zmag = 1;
+                Player1Score++;
+                if (Player1Score > Player1Highscore) Player1Highscore = Player1Score;
+            }
+            else if(Player1State==1 && Player2State == 2)
+            {
+                zmag = 1;
+                Player1Score++;
+                if (Player1Score > Player1Highscore) Player1Highscore = Player1Score;
+            }
+            else
+            {
+                zmag = 2;
+                Player2Score++;
+                if (Player2Score > Player2Highscore) Player2Highscore = Player2Score;
+            }
+#endregion
+            
+            
+            return zmag;
+        }
+    }
+}
